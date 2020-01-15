0a0dee8465a449f9fed85c59c6913d581dabe843
tajinder <59822139+github-solution-architect@users.noreply.github.com>
2020-01-15 15:16:59 +0100

    Update headers.js

--- headers.js
+++ headers.js
@@ -1,1 +1,14 @@
-
+/* Please note, Accept header is hard coded as per github documentation.
+       see thread here : https://github.com/octokit/rest.js/issues/1368
+       This might change in future and we need to update Accept header, but currently as per github documentaiton,
+       use : application/vnd.github.luke-cage-preview+json
+       for more information , please see https://developer.github.com/v3/repos/branches/#update-branch-protection */
+       
+exports.getData = function (repoName, githubToken) {
+  return {
+   "User-Agent":repoName,
+   "Authorization": 'Token '+githubToken,
+   "Content-Type": 'application/json',
+   "Accept":'application/vnd.github.luke-cage-preview+json'
+  };
+};
